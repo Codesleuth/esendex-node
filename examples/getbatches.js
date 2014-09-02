90011c9e8554f021a401412e3ff0cb073f0201da
David Wood <david.p.wood@gmail.com>
2014-09-02 22:13:59 +0100

    Adds batch rename.

--- examples/getbatches.js
+++ examples/getbatches.js
@@ -1,16 +1,24 @@
 var config = require('./config'),
+    format = require('util').format,
     esendex = require('../')(config);
 
 var options = {
   startIndex: 0,
-  count: 3,
+  count: 2,
   accountreference: config.accountreference
 };
 
 esendex.batches.get(options, function (err, batches) {
   if (err) return console.log(err);
 
-  batches.messagebatch.map(function (batch) {
+  batches.messagebatch.map(function (batch, index) {
     console.log(batch);
+
+    var newBatchName = format('My new name! (batch %d) %s', index, new Date());
+    esendex.batches.rename({ id: batch.id, name: newBatchName }, function (err) {
+      if (err) return console.log(err);
+
+      console.log('Batch renamed to: %s', newBatchName);
+    });
   });
 });
--- test/batches.test.js
+++ test/batches.test.js
@@ -152,4 +152,107 @@ describe('Batches', function () {
 
   });
 
+  describe('cancel when request error', function () {
+
+    var requestError;
+    var callbackSpy;
+
+    before(function () {
+      requestError = new Error('some request error');
+      var esendexFake = {
+        requesthandler: {
+          request: sinon.stub().callsArgWith(5, requestError)
+        }
+      };
+      callbackSpy = sinon.spy();
+
+      var Batches = proxyquire('../lib/batches', {});
+      var batches = Batches(esendexFake);
+      batches.cancel('asdasdda', callbackSpy);
+    });
+
+    it('should call the callback with the error', function () {
+      sinon.assert.calledWith(callbackSpy, requestError);
+    });
+
+  });
+
+  describe('rename', function () {
+
+    var requestXml;
+    var requestStub;
+    var options;
+    var expectedPath;
+    var callbackSpy;
+    var buildObjectStub;
+    var builderStub;
+
+    before(function () {
+      requestXml = 'batch rename body';
+      requestStub = sinon.stub().callsArgWith(5, null, '');
+      var esendexFake = {
+        requesthandler: {
+          request: requestStub
+        }
+      };
+      options = { id: '32c43729-6225-47f7-9359-521406fc29ac', name: 'My new batch name!' };
+      expectedPath = '/v1.1/messagebatches/' + options.id;
+      callbackSpy = sinon.spy();
+      buildObjectStub = sinon.stub().returns(requestXml);
+      builderStub = sinon.stub().returns(buildObjectStub);
+
+      var Batches = proxyquire('../lib/batches', { './xmlbuilder': builderStub });
+      var batches = Batches(esendexFake);
+      batches.rename(options, callbackSpy);
+    });
+
+    it('should create an xml builder with the messagebatch root element', function () {
+      sinon.assert.calledWith(builderStub, 'messagebatch');
+    });
+
+    it('should build the message batch rename request xml', function () {
+      sinon.assert.calledWith(buildObjectStub, sinon.match({
+        '$': {
+          xmlns: 'http://api.esendex.com/ns/'
+        },
+        name: options.name
+      }));
+    });
+
+    it('should call the messagebatches endpoint', function () {
+      sinon.assert.calledWith(requestStub, 'PUT', expectedPath, null, requestXml, 204, sinon.match.func);
+    });
+
+    it('should call the callback with the parsed messagebatches response', function () {
+      sinon.assert.calledWith(callbackSpy, null);
+    });
+
+  });
+
+  describe('rename when request error', function () {
+
+    var requestError;
+    var callbackSpy;
+
+    before(function () {
+      requestError = new Error('some request error');
+      var esendexFake = {
+        requesthandler: {
+          request: sinon.stub().callsArgWith(5, requestError)
+        }
+      };
+      callbackSpy = sinon.spy();
+      var buildObjectStub = sinon.stub().returns('asdsadasd');
+
+      var Batches = proxyquire('../lib/batches', { './xmlbuilder': sinon.stub().returns(buildObjectStub) });
+      var batches = Batches(esendexFake);
+      batches.rename('asdaaggadsgwq', callbackSpy);
+    });
+
+    it('should call the callback with the error', function () {
+      sinon.assert.calledWith(callbackSpy, requestError);
+    });
+
+  });
+
 });
