8cee91dff9f7982d2d4d0687239bc391f2739a37
David Wood <david.p.wood@gmail.com>
2015-11-18 22:24:07 +0000

    Use gulp to build and test and build to new build folder

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    merge = require('merge2'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rimraf = require('rimraf'),
+    mocha = require('gulp-mocha')
+    
+var paths = {
+  source: 'src',
+  root: 'build',
+  test: 'build/test',
+  definitions: 'build/definitions',
+  tsconfig: 'src/tsconfig.json',
+  sourceRoot: __dirname + '/src'
+}
+
+var tsconfig = {
+  module: 'commonjs',
+  removeComments: true,
+  declaration: true
+}
+
+gulp.task('clean', function (cb) {
+  rimraf(paths.root, cb);
+})
+
+gulp.task('build', ['clean'], function () {
+  var project = ts.createProject(paths.tsconfig, tsconfig)
+  
+  var tsResult = project.src()
+    .pipe(sourcemaps.init())
+    .pipe(ts(project))
+  
+  return merge([
+    tsResult.dts.pipe(gulp.dest(paths.definitions)),
+    tsResult.js.pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot })).pipe(gulp.dest(paths.root)),
+    gulp.src('package.json').pipe(gulp.dest(paths.root))
+  ])
+})
+
+gulp.task('test', function () {
+  return gulp.src(paths.test + '/**/*.js', { read: false })
+    // gulp-mocha needs filepaths so you can't have any plugins before it 
+    .pipe(mocha({ reporter: 'spec', require: ['source-map-support/register'] }));
+})
